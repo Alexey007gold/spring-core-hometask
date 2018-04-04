e2d7b00c6773989bedfd6d423c34de16a6bd5ff1
Oleksii_Kovetskyi <Oleksii_Kovetskyi@epam.com>
2018-04-04 10:54:59 +0300

    Added implementation of UserService (UserServiceImpl), added spring.xml

---
+++ src/main/java/ua/epam/spring/hometask/service/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package ua.epam.spring.hometask.service;
+
+import ua.epam.spring.hometask.domain.User;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.*;
+
+/**
+ * Created on 4/4/2018.
+ */
+public class UserServiceImpl implements UserService {
+
+    private Map<Long, User> userMap;
+
+    public UserServiceImpl() {
+        userMap = new HashMap<>();
+    }
+
+    @Nullable
+    @Override
+    public User getUserByEmail(@Nonnull String email) {
+        for (User user : userMap.values()) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public User save(@Nonnull User object) {
+        return userMap.put(object.getId(), object);
+    }
+
+    @Override
+    public void remove(@Nonnull User object) {
+        userMap.remove(object.getId());
+    }
+
+    @Override
+    public User getById(@Nonnull Long id) {
+        return userMap.get(id);
+    }
+
+    @Nonnull
+    @Override
+    public Collection<User> getAll() {
+        return new ArrayList<>(userMap.values());
+    }
+}
