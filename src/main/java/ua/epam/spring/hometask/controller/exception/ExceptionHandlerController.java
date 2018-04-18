aad6951cd9bd565fcb05317356e573e2df762129
Oleksii_Kovetskyi <Oleksii_Kovetskyi@epam.com>
2018-04-18 17:26:27 +0300

    Made ExceptionHandlerController use FreeMarker

--- src/main/java/ua/epam/spring/hometask/controller/exception/ExceptionHandlerController.java
+++ src/main/java/ua/epam/spring/hometask/controller/exception/ExceptionHandlerController.java
@@ -1,18 +1,21 @@
 package ua.epam.spring.hometask.controller.exception;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Oleksii_Kovetskyi on 4/18/2018.
  */
 @ControllerAdvice
-@RestController
+@Controller
 public class ExceptionHandlerController {
 
     @ExceptionHandler(Exception.class)
-    public String myError(Exception exception) {
-        return exception.getMessage();
+    public ModelAndView myError(Exception exception) {
+        ModelAndView mav = new ModelAndView("exception");
+        mav.addObject("message", exception.getMessage());
+        return mav;
     }
 }
---
+++ src/main/resources/template/exception.ftl
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="/resources/css/style.css">
+</head>
+<body>
+<div id="header">
+    <h4>Exception</h4>
+</div>
+<div id="content">
+    <p>${message}</p>
+</div>
+</body>
+</html>
