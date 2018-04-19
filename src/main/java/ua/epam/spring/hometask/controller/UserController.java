9c75549e9c2cd8b86bbf0978a6e13f980319b482
Oleksii_Kovetskyi <Oleksii_Kovetskyi@epam.com>
2018-04-19 19:38:26 +0300

    Updated UserController.upload() method to support user login, password and roles

--- src/main/java/ua/epam/spring/hometask/controller/UserController.java
+++ src/main/java/ua/epam/spring/hometask/controller/UserController.java
@@ -2,12 +2,15 @@ package ua.epam.spring.hometask.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ua.epam.spring.hometask.domain.User;
+import ua.epam.spring.hometask.domain.UserRole;
+import ua.epam.spring.hometask.service.interf.UserRoleService;
 import ua.epam.spring.hometask.service.interf.UserService;
 
 import java.io.BufferedReader;
@@ -15,6 +18,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,12 +30,16 @@ import java.util.List;
 public class UserController {
 
     private UserService userService;
+    private UserRoleService userRoleService;
+    private PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRoleService userRoleService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.userRoleService = userRoleService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @RequestMapping("/")
+    @RequestMapping
     public String getUsers(@ModelAttribute("model") ModelMap model,
                                  @RequestParam(required = false) String firstName,
                                  @RequestParam(required = false) String lastName,
@@ -71,13 +79,21 @@ public class UserController {
         String line;
         while ((line = reader.readLine()) != null) {
             String[] split = line.split(",");
-            LocalDate dateTime = new Timestamp(Long.parseLong(split[3])).toLocalDateTime().toLocalDate();
+            LocalDate dateTime = new Timestamp(Long.parseLong(split[5]) * 1000).toLocalDateTime().toLocalDate();
             User user = new User();
             user.setFirstName(split[0]);
             user.setLastName(split[1]);
             user.setEmail(split[2]);
+            user.setLogin(split[3]);
+            user.setPassword(passwordEncoder.encode(split[4]));
             user.setBirthDate(dateTime);
             userService.save(user);
+
+            List<UserRole> userRoles = new ArrayList<>();
+            for (int i = 6; i < split.length; i++) {
+                userRoles.add(new UserRole(user.getId(), split[i]));
+            }
+            userRoles.forEach(ur -> userRoleService.save(ur));
         }
         reader.close();
     }
